ca3c8de6927039b0cc4d87a1581a09430f56117e
charvi1 <charvi236.be22@chitkara.edu.in>
2024-08-30 20:54:00 +0530

    different component section made

---
+++ trial/src/Components/AboutHeader.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutHeader = () => {
+  return (
+    <div className="about-bg">
+      <div className="about-text-heading">
+        <h1>Pawsitively Wholesome</h1>
+        <h2>Innovation for every paw</h2>
+        <h5>FURS, FINS, WINGS & MORE</h5>
+      </div>
+      <img src="header-about.png" alt="About Header" />
+    </div>
+  );
+};
+
+export default AboutHeader;
--- trial/src/Pages/home.js
+++ trial/src/Pages/home.js
@@ -4,16 +4,19 @@ import '../App.css';
 import HeroSection from '../Components/HeroSection'; 
 import CategorySection from '../Components/CategorySection'; 
 import FeaturesSection from '../Components/FeaturesSection'; 
-import BrandsSection from '../Components/BrandsSection'; 
+import BrandsSection from '../Components/BrandsSection';
+import Header from '../Components/header'; // Adjust the path as needed
+import Footer from '../Components/footer';
 
 const Home = () => {
   return (
     <main>
+      <Header/>
       <HeroSection />
       <CategorySection />
       <FeaturesSection />
       <BrandsSection />
-      <footer/>
+      <Footer/>
     </main>
   );
 };
