618ffd8648161aa96167c7ac6bf83c1679aa383b
avireet <123054965+avireet@users.noreply.github.com>
2024-12-08 19:27:39 +0530

    rating

--- fuzzybackend/models/products.cjs
+++ fuzzybackend/models/products.cjs
@@ -12,6 +12,7 @@ const productSchema = new mongoose.Schema({
   category: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
+  rating:{type:Number,required:true},
   images: [{ type: String }],
   feedbacks: [feedbackSchema], // Embed feedback here
 });
