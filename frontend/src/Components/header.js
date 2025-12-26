733e2d3083e69fcdd0251700cc9f45d155429e6a
charvi1 <125857988+charvi1@users.noreply.github.com>
2025-12-26 10:11:24 +0530

    Refactor Header component structure and styling

--- frontend/src/Components/header.js
+++ frontend/src/Components/header.js
@@ -16,74 +16,76 @@ const Header = ({ isAuthenticated, isAdmin, onLogout }) => {
 
     return (
         <div className="header">
-            <div className="logo">
-                <img src="logo2.png" alt="Logo" />
-            </div>
-            <hr />
-            <nav>
-                <ul>
-                    <div className='navdivwologo'>
-                        <li><Link to="/">Home</Link></li>
-                        {!isAdmin && (
-                            <>
-                                <li><Link to="/products">Shop</Link></li>
-                                <li><Link to="/about">About Us</Link></li>
-                            </>
-                        )}
-                        {/* Contact Us removed */}
-                        {isAuthenticated && !isAdmin && <li><Link to="/orders">My Orders</Link></li>}
-
-                        {/* Show admin dashboard link only if isAdmin is true */}
-                        {isAdmin && (
-                            <li>
-                                <Link to="/admin" className="admin-link">
-                                    ADMIN DASHBOARD
-                                </Link>
-                            </li>
-                        )}
-                    </div>
-
-                    <div className='navdivwlogo'>
-                        {/* Show CART tab only if isAdmin is false */}
-                        {!isAdmin && (
-                            <li>
-                                <Link to="/cart">
-                                    <FaShoppingCart size={19} className='header-icons' aria-hidden="true" />
-                                </Link>
-                            </li>
-                        )}
-
-                        {/* Show BILLING tab with icon only if authenticated and NOT admin */}
-                        {isAuthenticated && !isAdmin && (
-                            <li>
-                                <Link to="/billing" className="billing-link">
-                                    <FaFileInvoiceDollar size={19} className='header-icons' aria-hidden="true" />
-                                </Link>
-                            </li>
-                        )}
-
-                        {isAuthenticated && (
-                            <li>
-                                <Link to="/profile">
-                                    <FaUserAlt size={17} className='header-icons' aria-hidden="true" />
-                                </Link>
-                            </li>
-                        )}
-
-                        <li>
-                            {isAuthenticated ? (
-                                <button onClick={handleLogoutClick} className="logout-button">
-                                    <AiOutlineLogout size={23} className='header-icons' aria-hidden="true" />
-                                </button>
-                            ) : (
-                                <Link to="/login">
-                                    <FaUserAlt size={16} className='header-icons' aria-hidden="true" />LOGIN
-                                </Link>
+            <div className="header-container">
+                <div className="logo">
+                     <Link to="/">
+                        <img src="logo2.png" alt="Logo" />
+                    </Link>
+                </div>
+                <nav>
+                    <ul className="nav-list">
+                        <div className='nav-links'>
+                            <li><Link to="/">Home</Link></li>
+                            {!isAdmin && (
+                                <>
+                                    <li><Link to="/products">Shop</Link></li>
+                                    <li><Link to="/about">About Us</Link></li>
+                                </>
                             )}
-                        </li>
-                    </div>
-                </ul>
-            </nav>
+                            {isAuthenticated && !isAdmin && <li><Link to="/orders">My Orders</Link></li>}
+
+                            {/* Show admin dashboard link only if isAdmin is true */}
+                            {isAdmin && (
+                                <li>
+                                    <Link to="/admin" className="admin-link">
+                                        ADMIN DASHBOARD
+                                    </Link>
+                                </li>
+                            )}
+                        </div>
+
+                        <div className='nav-icons'>
+                            {/* Show CART tab only if isAdmin is false */}
+                            {!isAdmin && (
+                                <li>
+                                    <Link to="/cart">
+                                        <FaShoppingCart size={19} className='header-icons' aria-hidden="true" />
+                                    </Link>
+                                </li>
+                            )}
+
+                            {/* Show BILLING tab with icon only if authenticated and NOT admin */}
+                            {isAuthenticated && !isAdmin && (
+                                <li>
+                                    <Link to="/billing" className="billing-link">
+                                        <FaFileInvoiceDollar size={19} className='header-icons' aria-hidden="true" />
+                                    </Link>
+                                </li>
+                            )}
+
+                            {isAuthenticated && (
+                                <li>
+                                    <Link to="/profile">
+                                        <FaUserAlt size={17} className='header-icons' aria-hidden="true" />
+                                    </Link>
+                                </li>
+                            )}
+
+                            <li>
+                                {isAuthenticated ? (
+                                    <button onClick={handleLogoutClick} className="logout-button">
+                                        <AiOutlineLogout size={23} className='header-icons' aria-hidden="true" />
+                                    </button>
+                                ) : (
+                                    <Link to="/login" className="login-link">
+                                        <FaUserAlt size={16} className='header-icons' aria-hidden="true" />LOGIN
+                                    </Link>
+                                )}
+                            </li>
+                        </div>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 };
